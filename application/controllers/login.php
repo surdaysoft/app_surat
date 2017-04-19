8938ec626a4aec832aa4570f0a64c3ea755e5db0
Iwan M Setiawan <surdaysoft@gmail.com>
2017-04-19 07:16:14 +0700

    Add Login Page
    fungsi login page

---
+++ application/controllers/login.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('login');
+	}
+
+	public function do_login()
+	{
+		$u = $this->input->post("user");
+		$p = md5($this->input->post("pass"));
+
+		// echo json_encode('user' => $user, 'pass' => $pass);
+		// echo $u."<br>".$p;
+
+		$cari = $this->model_admin->cek_login($u, $p)->row();
+		$hitung = $this->model_admin->cek_login($u, $p)->num_rows();
+
+		if ($hitung > 0) {
+			
+			$data = array('admin_id' => $cari->id_user ,
+							'admin_user' => $cari->username, 
+							'admin_nama' => $cari->nama,
+							'admin_valid' => TRUE
+			);
+
+			$this->session->set_userdata($data);
+
+			redirect('admin','refresh');
+		}else{
+			echo "maaf username atau password salah";
+		}
+		
+		
+		
+	}
+
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect('login');
+	}
+
+}
+
+/* End of file login.php */
+/* Location: ./application/controllers/login.php */
---
+++ application/views/login.php
@@ -0,0 +1,82 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>App Surat Menyurat  | Login Page</title>
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/icon.ico">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/iCheck/square/blue.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body class="hold-transition login-page">
+    <div class="login-box">
+      <!--
+      <div class="login-logo">
+        <a href="<?php echo base_url(); ?>"><b><?php echo ucwords($setting->nama_instansi); ?></b></a>
+      </div> /.login-logo -->
+      <!-- <div class="well well-sm">
+        <div class="row">
+          <div class="col-sm-12">
+            <img src="<?php echo base_url() ?>assets/upload/header.png" class="img img-responsive" style="display: inline; float: left; margin-right: 20px;">
+          </div>
+        </div>
+        
+      </div> -->
+      
+      <div class="login-box-body">
+        <p class="login-box-msg">Silahkan Login</p>
+        <?php echo $this->session->flashdata("k"); ?>
+        <?php echo form_open('login/do_login'); ?>
+          <div class="form-group has-feedback">
+            <input type="text" name="user" autofocus required="" class="form-control" placeholder="Username">
+            <span class="glyphicon glyphicon-user form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback">
+            <input type="password" name="pass" required="" class="form-control" placeholder="Password">
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <div class="row">
+            <div class="col-xs-4">
+              <button type="submit" class="btn btn-primary btn-block btn-flat"><span class="fa fa-sign-in"></span> Log In</button>
+            </div><!-- /.col -->
+          </div>
+        <?php echo form_close(); ?>
+
+      </div><!-- /.login-box-body -->
+    </div><!-- /.login-box -->
+
+    <!-- jQuery 2.1.4 -->
+    <script src="<?php echo base_url(); ?>assets/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
+    <!-- iCheck -->
+    <script src="<?php echo base_url(); ?>assets/plugins/iCheck/icheck.min.js"></script>
+    <script>
+      $(function () {
+        $('input').iCheck({
+          checkboxClass: 'icheckbox_square-blue',
+          radioClass: 'iradio_square-blue',
+          increaseArea: '20%' // optional
+        });
+      });
+    </script>
+  </body>
+</html>
